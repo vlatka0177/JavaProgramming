ea0c194e7a32cbc819e5a34bfe7d355296c0f624
vlatkapersin <vlatka.persin@gmail.com>
2022-01-19 14:30:25 -0500

    Update

---
+++ OfficeHours/day08/ObjectNum.java
@@ -0,0 +1,17 @@
+package OfficeHours.day08;
+
+public class ObjectNum {
+
+    public static void main(String[] args) {
+
+        ObjectNum on1 = new ObjectNum();
+        ObjectNum on2 = on1;
+        ObjectNum on3 ;
+        on3 = new ObjectNum();
+        ObjectNum on4 = on3;
+        on4 = new ObjectNum();
+
+        // 3 new objects are created (new keyword).
+    }
+
+}
