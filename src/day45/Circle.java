0ab9a8004891e67575329043ea95dab5098e7d7d
vlatkapersin <vlatka.persin@gmail.com>
2022-02-07 15:06:14 -0500

    Day45 Java

---
+++ src/day45/Circle.java
@@ -0,0 +1,36 @@
+package day45;
+
+public class Circle extends Shape {
+
+    private double radius; // instance because each circle has a different radius
+    public final static double pi =3.14; // pi value is unchangeable, and the same for all circles
+
+    // Generate a constructor
+    public Circle(String name, double radius) {
+        super("Circle"); // change name to "Circle"
+        this.radius = radius;
+    }
+
+    // Generate getter
+    public double getRadius() {
+        return radius;
+    }
+
+    // Generate setter
+    public void setRadius(double radius) {
+        if (radius < 0) {
+            throw new RuntimeException("Invalid Radius: " + radius);
+        }
+            this.radius = radius;
+        }
+
+    @Override
+    public double area() {
+        return radius * radius * pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * radius * pi;
+    }
+}
---
+++ src/day45/Shape.java
@@ -0,0 +1,32 @@
+package day45;
+
+public abstract class Shape {
+
+    private final String name;
+
+    // Parent class provides variables and methods. Child class implements these variables and methods.
+
+    // Generate a constructor
+    public Shape(String name) {
+        // this.name = name                      // Option 1
+        this.name = getClass().getSimpleName();  // Option 2
+    }
+
+    public String getName() { // final can generate only a getter, not a setter
+        return name;
+    }
+
+    public abstract double area();
+
+    public abstract double perimeter();
+
+    // Generate toString method
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +        // Delete "Shape". Insert getClass().getSimpleName()
+                "name='" + name + '\'' +
+                ", area='" + area() + '\'' +             // Add area
+                ", perimeter='" + perimeter() + '\'' +   // Add perimeter
+                '}';
+    }
+}
