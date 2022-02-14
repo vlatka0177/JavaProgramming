1384469130a5058428de2ae4fb1a5b214008de34
vlatkapersin <vlatka.persin@gmail.com>
2022-02-13 20:19:06 -0500

    Day46 Java

---
+++ src/day46_Polymorphism/PolymorphismIntro.java
@@ -0,0 +1,19 @@
+package day46_Polymorphism;
+
+public class PolymorphismIntro {
+
+    public static void main(String[] args) {
+
+        /*
+        POLYMORPHISM (MANY FORMS)
+
+        - ability of the objects to take on many forms
+        - reference type is a parent class
+        - interface and object class is child
+
+
+         */
+
+    }
+
+}
