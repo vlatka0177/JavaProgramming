1152bc1261d7abfbe6d30520daf5cabd0ff9ab9d
vlatkapersin <vlatka.persin@gmail.com>
2022-04-14 16:12:42 -0400

    Day49 Java

--- InterviewQuestions/MoveZerosToTheEnd.java
+++ InterviewQuestions/MoveZerosToTheEnd.java
@@ -7,8 +7,11 @@ public class MoveZerosToTheEnd {
     public static void main(String[] args) {
 
         int[] arr = {0, 1, 0, 2, 0, 3, 0, 4, 0, 5};
-        int[] result = moveZerosToTheEnd(arr);
-        System.out.println(Arrays.toString(result));
+
+        // int[] result = moveZerosToTheEnd(arr);
+        // System.out.println(Arrays.toString(result));
+
+        System.out.println(Arrays.toString(moveZerosToTheEnd(arr)));
     }
 
     public static int[] moveZerosToTheEnd(int[] array) {
